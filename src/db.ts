83bcad7b3c62ecbb44db4be44bdb1a8f1ec49d30
rabbit19981023 <rabbit19981023@gmail.com>
2021-02-26 22:42:30 +0800

    fix dependencies & rename variables

--- src/db.ts
+++ src/db.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export const connectDB = (): void => {
 	// Database Config (MongoDB)
 	const dbConfig = {
-		url: process.env.MONGODB_URL || "mongodb+srv://admin:smile123@cluster-test.3y2n1.mongodb.net/Cluster-test?retryWrites=true&w=majority",
+		uri: process.env.MONGODB_URI || "mongodb+srv://admin:smile123@cluster-test.3y2n1.mongodb.net/Cluster-test?retryWrites=true&w=majority",
 		params: {
 			useNewUrlParser: true,
 			useCreateIndex: true,
@@ -12,7 +12,7 @@ export const connectDB = (): void => {
 	}
 
 	// connecting to database (MongoDB)
-	mongoose.connect(dbConfig.url, dbConfig.params)
+	mongoose.connect(dbConfig.uri, dbConfig.params)
 		.then(() => {
 			console.log('Connecting to database successfully!');
 		})
